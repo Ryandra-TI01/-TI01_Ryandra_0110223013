5db02c594273dc271047134afd6b9c16f64eecc5
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2024-06-11 22:31:27 +0700

    praktikum 13

---
+++ semester2/praktikum_9/routes/web copy.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PasienController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// // routing baru
+// Route::get('/salam',function(){
+//     return "Selamat pagi ges";
+// });
+// // routing profil
+// Route::get('/profil',function(){
+//     return view('profil');
+// });
+// // routing about
+// Route::get('/about',function(){
+//     return view('about');
+// });
+// // routing about
+// Route::get('/dashboard',[DashboardController::class,'show']);
+
+
+
+// dashboar route
+route::get('admin/dashboard',[DashboardController::class,'index']);
+
+
+// ------ index course --------
+Route::get('admin/courses',[CoursesController::class,'index']);
+// menampilkan data 
+Route::get('admin/courses/create',[CoursesController::class,'create']);
+Route::post('admin/courses/create',[CoursesController::class,'store']);
+// edit data
+Route::get('admin/courses/edit/{id}',[CoursesController::class,'edit'])->name('courses.edit');
+// update data
+Route::put('admin/courses/update/{id}',[CoursesController::class,'update']);
+// menghapus data
+Route::delete('admin/courses/delete/{id}',[CoursesController::class,'destroy']);
+
+
+// ------ index student --------
+Route::get('admin/student',[StudentController::class,'index']);
+// menampilkan data 
+Route::get('admin/student/create',[StudentController::class,'create']);
+Route::post('admin/student/create',[StudentController::class,'store']);
+// edit data
+Route::get('admin/student/edit/{id}',[StudentController::class,'edit'])->name('student.edit');
+// update data
+Route::put('admin/student/update/{id}',[StudentController::class,'update']);
+// menghapus data
+Route::delete('admin/student/delete/{id}',[StudentController::class,'destroy']);
--- semester2/praktikum_9/routes/web.php
+++ semester2/praktikum_9/routes/web.php
@@ -1,33 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PasienController;
 use App\Http\Controllers\StudentController;
-use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// // routing baru
-// Route::get('/salam',function(){
-//     return "Selamat pagi ges";
-// });
-// // routing profil
-// Route::get('/profil',function(){
-//     return view('profil');
-// });
-// // routing about
-// Route::get('/about',function(){
-//     return view('about');
-// });
-// // routing about
-// Route::get('/dashboard',[DashboardController::class,'show']);
-
 
 
 // dashboar route
-route::get('admin/dashboard',[DashboardController::class,'index']);
+route::get('admin/dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
 
 
 // ------ index course --------
@@ -54,3 +36,44 @@ Route::get('admin/student/edit/{id}',[StudentController::class,'edit'])->name('s
 Route::put('admin/student/update/{id}',[StudentController::class,'update']);
 // menghapus data
 Route::delete('admin/student/delete/{id}',[StudentController::class,'destroy']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // ------ index course --------
+    Route::get('admin/courses',[CoursesController::class,'index']);
+    // menampilkan data 
+    Route::get('admin/courses/create',[CoursesController::class,'create']);
+    Route::post('admin/courses/create',[CoursesController::class,'store']);
+    // edit data
+    Route::get('admin/courses/edit/{id}',[CoursesController::class,'edit'])->name('courses.edit');
+    // update data
+    Route::put('admin/courses/update/{id}',[CoursesController::class,'update']);
+    // menghapus data
+    Route::delete('admin/courses/delete/{id}',[CoursesController::class,'destroy']);
+
+
+    // ------ index student --------
+    Route::get('admin/student',[StudentController::class,'index']);
+    // menampilkan data 
+    Route::get('admin/student/create',[StudentController::class,'create']);
+    Route::post('admin/student/create',[StudentController::class,'store']);
+    // edit data
+    Route::get('admin/student/edit/{id}',[StudentController::class,'edit'])->name('student.edit');
+    // update data
+    Route::put('admin/student/update/{id}',[StudentController::class,'update']);
+    // menghapus data
+    Route::delete('admin/student/delete/{id}',[StudentController::class,'destroy']);
+});
+
+require __DIR__.'/auth.php';
